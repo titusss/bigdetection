8c98b06ddb1982ad636b148c437c19942a9df76c
titusss <26855197+titusss@users.noreply.github.com>
2022-10-13 14:00:08 -0700

    Removed gt_semantic_seg from facsed config

--- configs/BigDetection/cbnetv2/htc_cbv2_swin_base_adamw_20e_facsed.py
+++ configs/BigDetection/cbnetv2/htc_cbv2_swin_base_adamw_20e_facsed.py
@@ -68,7 +68,7 @@ train_pipeline = [
     dict(type="DefaultFormatBundle"),
     dict(
         type="Collect",
-        keys=["img", "gt_bboxes", "gt_labels", "gt_masks", "gt_semantic_seg"],
+        keys=["img", "gt_bboxes", "gt_labels", "gt_masks"],
     ),
 ]
 test_pipeline = [
